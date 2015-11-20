588ba81a0c18359b92cbec5b49e94046f5927ada
john <07jfxiao@gmail.com>
2015-11-20 16:10:53 +0800

    Fix bug of mix error index

--- expand_to_css_selector.py
+++ expand_to_css_selector.py
@@ -11,14 +11,15 @@ except:
 def expand_to_css_selector(string, start, end):
 
   view = sublime.Window.active_view(sublime.active_window())
+  line_start = view.line(view.sel()[0]).begin()
   cur_line = view.substr(view.line(view.sel()[0]))
   # print len(cur_line)
   has_seletor = re.compile(r'\s*([\.\#]{1}[^\s].*[^\s])*\s*[\{|,]')
   matched = has_seletor.match(cur_line)
   if matched:
     # Retrieve the groups(1)'s start and end.
-    newStartIndex = matched.start(1)
-    newEndIndex = matched.end(1)
+    newStartIndex = matched.start(1) + line_start
+    newEndIndex = matched.end(1) + line_start
 
     if (start != newStartIndex and end != newEndIndex):
       print newStartIndex, newEndIndex
