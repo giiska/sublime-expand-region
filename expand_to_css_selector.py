36af4e7f63dd1f1c918cfee18a1abdb80f3e5f5a
john <07jfxiao@gmail.com>
2015-11-23 16:09:25 +0800

    Upd css selector reg rule for exclude selector prefix

--- expand_to_css_selector.py
+++ expand_to_css_selector.py
@@ -14,13 +14,14 @@ def expand_to_css_selector(string, start, end):
   line_start = view.line(view.sel()[0]).begin()
   cur_line = view.substr(view.line(view.sel()[0]))
   # print len(cur_line)
-  has_seletor = re.compile(r'\s*([\.\#]{1}[^\s].*[^\s])*\s*[\{|,]')
+  has_seletor = re.compile(r'\s*[\.\#]{1}([^\s].*[^\s])*\s*[\{|,]')
   matched = has_seletor.match(cur_line)
   if matched:
     # Retrieve the groups(1)'s start and end.
     newStartIndex = matched.start(1) + line_start
     newEndIndex = matched.end(1) + line_start
 
+    # if is not equal current selection
     if (start != newStartIndex and end != newEndIndex):
       print newStartIndex, newEndIndex
       return utils.create_return_obj(newStartIndex, newEndIndex, string, "css_selector")
